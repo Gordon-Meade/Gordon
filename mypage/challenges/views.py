e3d6819cda3daa2ea3536c7340f0f517729eaccb
Gordon Meade <gordonmeade@gmail.com>
2025-01-16 11:08:21 +0000

    Add dynamic features to monthly challenge views to suppport challenges.html page

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse 
 
+
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
     "february": "Walk for at least 20 minutes every day",
@@ -45,8 +46,10 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "month_name": month.capitalize()
+            })
     except:
         return HttpResponseNotFound("This month is not supported!")
 
